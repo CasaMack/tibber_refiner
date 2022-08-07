583b2a9b8a732d806cddb4beddeff97d7b2bbb92
Areskiko <39183620+Areskiko@users.noreply.github.com>
2022-08-07 12:48:30 +0200

    Fix loop

--- src/main.rs
+++ src/main.rs
@@ -11,31 +11,23 @@ async fn main() {
     let (db_addr, db_name) = get_db_info();
     let retries = get_retries();
 
-    let res = tick(db_addr.clone(), db_name.clone()).await;
-    match res {
-        Ok(_) => {}
-        Err(e) => {
-            tracing::error!("{}", e)
+    loop {
+        let instant = get_instant();
+        time::sleep_until(instant).await;
+        for i in 0..retries {
+            let res = tick(db_addr.clone(), db_name.clone()).await;
+            if res.is_ok() {
+                break;
+            } else {
+                tracing::warn!("Failed attempt {} to tick: {}", i, res.err().unwrap());
+                let backoff = 2_u64.pow(i);
+                tracing::debug!("Exponential backoff: {} seconds", backoff);
+                time::sleep(time::Duration::from_secs(backoff)).await;
+            }
+            tracing::error!(
+                "Unable to refine values after {} retires. Giving up",
+                retries
+            );
         }
     }
-    //    loop {
-    //        let instant = get_instant();
-    //        time::sleep_until(instant).await;
-    //        for i in 0..retries {
-    //            let res = tick(
-    //                db_addr.clone(),
-    //                db_name.clone(),
-    //            )
-    //            .await;
-    //            if res.is_ok() {
-    //                break;
-    //            } else {
-    //                tracing::warn!("Failed attempt {} to tick: {}", i, res.err().unwrap());
-    //                let backoff = 2_u64.pow(i);
-    //                tracing::debug!("Exponential backoff: {} seconds", backoff);
-    //                time::sleep(time::Duration::from_secs(backoff)).await;
-    //            }
-    //        tracing::error!("Unable to refine values after {} retires. Giving up", retries);
-    //        }
-    //    }
 }
